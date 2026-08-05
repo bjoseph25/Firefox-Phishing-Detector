aa774dfa0b22f2753728b6147b7d2ee0c932bf8c
bjoseph25 <brandonjosephjobs@gmail.com>
2026-08-04 23:02:22 -0400

    Added URL obfuscation detection heuristics

--- content.js
+++ content.js
@@ -1,3 +1,38 @@
+const currentUrl = window.location.href;
+
+const hasAtSymbol = detectAtSymbol(currentUrl);
+
+const hostnameData = analyzeHostname(currentUrl);
+
+const suspiciousKeywords = [
+    "login",
+    "signin",
+    "verify",
+    "verification",
+    "secure",
+    "account",
+    "update",
+    "password",
+    "confirm",
+    "unlock"
+];
+
+console.log("Current URL:", currentUrl);
+
+console.log(detectAtSymbol(currentUrl));
+
+console.log(detectEncodedCharacters(currentUrl));
+
+console.log(detectLongUrl(currentUrl));
+
+console.log(detectRepeatedSlashes(currentUrl));
+
+console.log(analyzeHostname(currentUrl));
+
+console.log(countSubdomains(hostnameData.hostname));
+
+console.log(detectSuspiciousKeywords(currentUrl));
+
 console.log("CONTENT SCRIPT RUNNING")
 
 function detectLoginForm() {
@@ -12,3 +47,62 @@ function detectLoginForm() {
 }
 
 console.log(detectLoginForm());
+
+function detectAtSymbol(url) {
+    return {
+        hasAtSymbol: url.includes("@")
+    };
+}
+
+function detectEncodedCharacters(url) {
+    const matches = url.match(/%[0-9A-Fa-f]{2}/g);
+
+    return {
+        encodedCharacterCount: matches ? matches.length : 0
+    };
+}
+
+function detectLongUrl(url) {
+    return {
+        urlLength: url.length,
+        isLongUrl: url.length > 100
+    };
+}
+
+function detectRepeatedSlashes(url) {
+    const urlWithoutProtocol = url.replace(/^https?:\/\//,"");
+
+    return {
+        hasRepeatedSlashes: /\{2,}/.test(urlWithoutProtocol)
+    };
+}
+
+function analyzeHostname(url) {
+    const parsedUrl = new URL(url);
+
+    return {
+        hostname: parsedUrl.hostname
+    };
+}
+
+function countSubdomains(hostname) {
+    const parts = hostname.split(".");
+
+    return {
+        hostname: hostname,
+        subdomainCount: parts.length - 2
+    };
+}
+
+function detectSuspiciousKeywords(url) {
+    const lowerUrl = url.toLowerCase();
+
+    const foundKeywords = suspiciousKeywords.filter(keyword =>
+        lowerUrl.includes(keyword)
+    );
+
+    return {
+        foundKeywords: foundKeywords,
+        keywordCount: foundKeywords.length
+    };
+}
